158bddda1780baba71019de8066ad6425bd13448
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-20 20:24:05 +0900

    FEAT: Spring Boot Relay 서버 메인 애플리케이션 클래스를 추가한다.

---
+++ src/main/java/com/woori/woorirelay/WooriRelayApplication.java
@@ -0,0 +1,13 @@
+package com.woori.woorirelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriRelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooriRelayApplication.class, args);
+    }
+
+}
