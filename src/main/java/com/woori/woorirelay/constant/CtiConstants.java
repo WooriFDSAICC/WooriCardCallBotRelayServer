6630cf970a6bfb85f4265ea9cf328a3643552d5b
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-20 20:24:05 +0900

    CHORE: CTI 에스컬레이션 API 경로·타임아웃 상수를 정의한다.

---
+++ src/main/java/com/woori/woorirelay/constant/CtiConstants.java
@@ -0,0 +1,11 @@
+package com.woori.woorirelay.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class CtiConstants {
+
+    public static final String DEFAULT_ESCALATION_PATH = "/api/v1/escalations";
+    public static final String HEADER_CORRELATION_ID = "X-Correlation-Id";
+    public static final String PRIORITY_HIGH = "HIGH";
+}
