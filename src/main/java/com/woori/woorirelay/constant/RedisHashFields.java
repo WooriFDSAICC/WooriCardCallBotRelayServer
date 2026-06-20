e3d3f8bb4c8320f5ef5e58dad1542ab6632b62ba
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-20 20:24:05 +0900

    CHORE: Redis 세션 Hash 필드명 상수를 정의한다. (#2)

---
+++ src/main/java/com/woori/woorirelay/constant/RedisHashFields.java
@@ -0,0 +1,14 @@
+package com.woori.woorirelay.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class RedisHashFields {
+
+    public static final String SESSION_ID = "session_id";
+    public static final String STATUS = "status";
+    public static final String FDS_FLAG = "fds_flag";
+    public static final String LAST_EVENT = "last_event";
+    public static final String LAST_STT_TEXT = "last_stt_text";
+    public static final String UPDATED_AT = "updated_at";
+}
