8bdc1ed9c880faec02714014b4cebe155d161842
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-22 18:20:35 +0900

    FEAT: Redis 세션 해시 필드에 callDirection·campaignId를 추가한다. (#3)

--- src/main/java/com/woori/woorirelay/constant/RedisHashFields.java
+++ src/main/java/com/woori/woorirelay/constant/RedisHashFields.java
@@ -1,3 +1,23 @@
+/**
+ *
+ *
+ * <pre>
+ * <b>Description  : Redis 세션 Hash 필드명</b>
+ * <b>Project Name : WooriCardCallBotRelayServer</b>
+ * package  : com.woori.woorirelay.constant
+ * </pre>
+ *
+ * @author : RosieOh
+ * @version : 1.0
+ * @since
+ *     <pre>
+ * Modification Information
+ *    수정일              수정자                수정내용
+ * ---------------   ---------------   ----------------------------
+ *  2026.06.22        RosieOh     최초생성
+ *        </pre>
+ */
+
 package com.woori.woorirelay.constant;
 
 import lombok.experimental.UtilityClass;
@@ -6,6 +26,8 @@ import lombok.experimental.UtilityClass;
 public final class RedisHashFields {
 
     public static final String SESSION_ID = "session_id";
+    public static final String CALL_DIRECTION = "call_direction";
+    public static final String CAMPAIGN_ID = "campaign_id";
     public static final String STATUS = "status";
     public static final String FDS_FLAG = "fds_flag";
     public static final String LAST_EVENT = "last_event";
