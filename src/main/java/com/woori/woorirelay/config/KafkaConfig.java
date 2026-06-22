252a76ecb62925d75bd8458dc2f7800b3aa8eda4
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-22 18:20:34 +0900

    FEAT: Kafka Producer 직렬화·토픽 설정을 FdsEvent 연동에 맞게 확장한다. (#2)

--- src/main/java/com/woori/woorirelay/config/KafkaConfig.java
+++ src/main/java/com/woori/woorirelay/config/KafkaConfig.java
@@ -1,3 +1,23 @@
+/**
+ *
+ *
+ * <pre>
+ * <b>Description  : Kafka Producer 설정</b>
+ * <b>Project Name : WooriCardCallBotRelayServer</b>
+ * package  : com.woori.woorirelay.config
+ * </pre>
+ *
+ * @author : RosieOh
+ * @version : 1.0
+ * @since
+ *     <pre>
+ * Modification Information
+ *    수정일              수정자                수정내용
+ * ---------------   ---------------   ----------------------------
+ *  2026.06.22        RosieOh     최초생성
+ *        </pre>
+ */
+
 package com.woori.woorirelay.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
