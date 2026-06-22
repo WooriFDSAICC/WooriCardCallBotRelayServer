c1920739945f093716632386e63f7bd7a4894d8f
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-22 18:20:34 +0900

    CHORE: Relay 애플리케이션 진입점에 스케줄링·메트릭 활성화를 반영한다.

--- src/main/java/com/woori/woorirelay/WooriRelayApplication.java
+++ src/main/java/com/woori/woorirelay/WooriRelayApplication.java
@@ -1,3 +1,23 @@
+/**
+ *
+ *
+ * <pre>
+ * <b>Description  : Spring Boot 애플리케이션 진입점</b>
+ * <b>Project Name : WooriCardCallBotRelayServer</b>
+ * package  : com.woori.woorirelay
+ * </pre>
+ *
+ * @author : RosieOh
+ * @version : 1.0
+ * @since
+ *     <pre>
+ * Modification Information
+ *    수정일              수정자                수정내용
+ * ---------------   ---------------   ----------------------------
+ *  2026.06.22        RosieOh     최초생성
+ *        </pre>
+ */
+
 package com.woori.woorirelay;
 
 import org.springframework.boot.SpringApplication;
