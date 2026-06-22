c9bf64ae4f56e7b944163e24d2eaed8400f202f4
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-22 18:20:36 +0900

    CHORE: Stream 이벤트 타입 상수를 IntegrationContracts와 동기화한다. (#2)

--- src/main/java/com/woori/woorirelay/constant/StreamEventTypes.java
+++ src/main/java/com/woori/woorirelay/constant/StreamEventTypes.java
@@ -1,11 +1,35 @@
+/**
+ *
+ *
+ * <pre>
+ * <b>Description  : 스트림 이벤트 타입 상수 (IntegrationContracts 위임)</b>
+ * <b>Project Name : WooriCardCallBotRelayServer</b>
+ * package  : com.woori.woorirelay.constant
+ * </pre>
+ *
+ * @author : RosieOh
+ * @version : 1.0
+ * @since
+ *     <pre>
+ * Modification Information
+ *    수정일              수정자                수정내용
+ * ---------------   ---------------   ----------------------------
+ *  2026.06.22        RosieOh     최초생성
+ *        </pre>
+ */
+
 package com.woori.woorirelay.constant;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * Relay 내부 이벤트 타입 — {@link IntegrationContracts} 위임.
+ * 기존 import 호환용 래퍼이며, 신규 코드는 IntegrationContracts를 직접 참조해도 된다.
+ */
 @UtilityClass
 public final class StreamEventTypes {
 
-    public static final String STT_PARTIAL = "STT_PARTIAL";
-    public static final String AGENT_ESCALATION = "AGENT_ESCALATION";
-    public static final String SESSION_ENDED = "SESSION_ENDED";
+    public static final String STT_PARTIAL = IntegrationContracts.EVENT_STT_PARTIAL;
+    public static final String AGENT_ESCALATION = IntegrationContracts.EVENT_AGENT_ESCALATION;
+    public static final String SESSION_ENDED = IntegrationContracts.EVENT_SESSION_ENDED;
 }
