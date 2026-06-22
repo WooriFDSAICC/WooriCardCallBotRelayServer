d9aa8c67ff6bd6217d117d75c515b53df465fe41
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-22 18:20:35 +0900

    CHORE: CTI Outbox 재시도·분산 세션 하트비트 스케줄링 설정을 추가한다. (#13)

---
+++ src/main/java/com/woori/woorirelay/config/RelaySchedulingConfig.java
@@ -0,0 +1,29 @@
+/**
+ *
+ *
+ * <pre>
+ * <b>Description  : 스케줄링 활성화 설정</b>
+ * <b>Project Name : WooriCardCallBotRelayServer</b>
+ * package  : com.woori.woorirelay.config
+ * </pre>
+ *
+ * @author : RosieOh
+ * @version : 1.0
+ * @since
+ *     <pre>
+ * Modification Information
+ *    수정일              수정자                수정내용
+ * ---------------   ---------------   ----------------------------
+ *  2026.06.22        RosieOh     최초생성
+ *        </pre>
+ */
+
+package com.woori.woorirelay.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class RelaySchedulingConfig {
+}
