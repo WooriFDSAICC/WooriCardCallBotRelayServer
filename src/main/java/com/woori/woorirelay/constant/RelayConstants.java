3d2f548fd56eb4ca01e288cf0851615861878475
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-22 18:20:35 +0900

    CHORE: Relay 내부 상수를 IntegrationContracts 위임 구조에 맞게 정비한다. (#2)

--- src/main/java/com/woori/woorirelay/constant/RelayConstants.java
+++ src/main/java/com/woori/woorirelay/constant/RelayConstants.java
@@ -1,3 +1,23 @@
+/**
+ *
+ *
+ * <pre>
+ * <b>Description  : Relay 기본 상수</b>
+ * <b>Project Name : WooriCardCallBotRelayServer</b>
+ * package  : com.woori.woorirelay.constant
+ * </pre>
+ *
+ * @author : RosieOh
+ * @version : 1.0
+ * @since
+ *     <pre>
+ * Modification Information
+ *    수정일              수정자                수정내용
+ * ---------------   ---------------   ----------------------------
+ *  2026.06.22        RosieOh     최초생성
+ *        </pre>
+ */
+
 package com.woori.woorirelay.constant;
 
 import lombok.experimental.UtilityClass;
@@ -8,11 +28,16 @@ public final class RelayConstants {
     public static final long FASTAPI_CONNECT_TIMEOUT_SECONDS = 10L;
     public static final long REDIS_SESSION_TTL_HOURS = 24L;
 
-    public static final String DEFAULT_KAFKA_TOPIC = "wooricard-fds-events";
+    public static final String DEFAULT_KAFKA_TOPIC = IntegrationContracts.TOPIC_FDS_EVENTS;
     public static final String DEFAULT_REDIS_SESSION_KEY_PREFIX = "wooricard:session:";
     public static final String DEFAULT_FASTAPI_WS_BASE_URL = "ws://localhost:8000/v1/stream";
+    public static final String DEFAULT_FASTAPI_WS_INBOUND_BASE_URL = "ws://localhost:8000/v1/stream/inbound";
+    public static final String DEFAULT_FASTAPI_WS_OUTBOUND_BASE_URL = "ws://localhost:8000/v1/stream/outbound";
 
     public static final int DEFAULT_MAX_BINARY_CHUNK_BYTES = 65_536;
     public static final int DEFAULT_WS_TEXT_BUFFER_SIZE = 65_536;
     public static final int DEFAULT_WS_BINARY_BUFFER_SIZE = 65_536;
+
+    public static final long CTI_OUTBOX_TTL_HOURS = 24L;
+    public static final long CTI_ESCALATION_DONE_TTL_HOURS = 24L;
 }
